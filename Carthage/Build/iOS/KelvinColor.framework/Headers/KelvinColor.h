3438dfadb9bd623383d1e415f8455619dbe1c4cf
Sihao Lu <lsh32768@gmail.com>
2017-12-09 12:47:48 -0800

    Check in Carthage/Build for faster CI

---
+++ Carthage/Build/iOS/KelvinColor.framework/Headers/KelvinColor.h
@@ -0,0 +1,19 @@
+//
+//  KelvinColor.h
+//  KelvinColor
+//
+//  Created by Sihao Lu on 8/24/17.
+//  Copyright © 2017 Sihao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KelvinColor.
+FOUNDATION_EXPORT double KelvinColorVersionNumber;
+
+//! Project version string for KelvinColor.
+FOUNDATION_EXPORT const unsigned char KelvinColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KelvinColor/PublicHeader.h>
+
+
---
+++ Carthage/Build/iOS/MathUtil.framework/Headers/MathUtil.h
@@ -0,0 +1,19 @@
+//
+//  MathUtil.h
+//  MathUtil
+//
+//  Created by Sihao Lu on 7/21/17.
+//  Copyright © 2017 Sihao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MathUtil.
+FOUNDATION_EXPORT double MathUtilVersionNumber;
+
+//! Project version string for MathUtil.
+FOUNDATION_EXPORT const unsigned char MathUtilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MathUtil/PublicHeader.h>
+
+
