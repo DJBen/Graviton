aada242db655636d643a45230f2aa03c668e0a63
Sihao Lu <lsh32768@gmail.com>
2017-12-20 17:43:30 -0800

    Add warning message at start up about SceneKit bug for iOS 11.2 users.

--- Graviton/Common/Utils/Device.swift
+++ Graviton/Common/Utils/Device.swift
@@ -13,4 +13,17 @@ struct Device {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         return appDelegate.window!.safeAreaInsets != UIEdgeInsets.zero
     }
+
+    /// Starting from `iOS 11.2.*`, the SceneKit seemingly does not render transparent textures.
+    ///
+    /// - seealso: [This](https://forums.developer.apple.com/thread/92671) apple forum thread.
+    static var isSceneKitBroken: Bool {
+        let os = ProcessInfo().operatingSystemVersion
+        switch (os.majorVersion, os.minorVersion, os.patchVersion) {
+        case (11, 2, _):
+            return true
+        default:
+            return false
+        }
+    }
 }
